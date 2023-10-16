f3308abd91c2f032b6fdccc3de2d09f313efd04b
HarrySkySon <123_4567@ukr.net>
2023-10-16 14:30:08 +0200

    Add function to prevent the CLI window from closing automatically after running the program

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
 use std::io;
 
 fn main() {
-    // Read input from user
-    let mut input = String::new();
-    println!("Enter a positive integer:");
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    let n: usize = input.trim().parse().expect("Invalid input");
 
-    // Find divisors
+    loop {
+
+        let mut input = String::new();
+
+        println!("Enter a positive integer (or q to quit): ");
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let n: usize = match input.trim() {
+            "q" => break,
+            num => num.parse().expect("Invalid number")
+        };
+
+        let divisors = find_divisors(n);
+
+        println!("Divisors of {}: {:?}", n, divisors);
+
+    }
+
+}
+
+fn find_divisors(n: usize) -> Vec<usize> {
+
     let mut divisors = Vec::new();
     let sqrt_n = (n as f64).sqrt() as usize;
+
     for i in 1..=sqrt_n {
         if n % i == 0 {
             divisors.push(i);
@@ -19,7 +39,6 @@ fn main() {
         }
     }
 
-    // Print results
     divisors.sort();
-    println!("Divisors of {}: {:?}", n, divisors);
+    divisors
 }
